4c04b32b5028295a2e884539ae81cbdcb8a70bc9
Septimiu Draghici <septimiu.draghici@principal33.com>
2021-09-01 12:48:50 +0300

    Moved Project to dedicated folder

--- AspNetSandBox/Controllers/WeatherForecastController.cs
+++ AspNetSandBox/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace AspNetSandBox.Controllers
 {
@@ -26,6 +27,28 @@ namespace AspNetSandBox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=46.4760&lon=24.0934&exclude=hourly,minutely&appid=33a7b8b2ffb63bb3514e58a43a77ccf9");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            return ConvertResponseToWeatherForecast(response.Content);
+
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+
+            //https://api.openweathermap.org/data/2.5/onecall?lat=46.4760&lon=24.0934&exclude=hourly,minutely&appid=33a7b8b2ffb63bb3514e58a43a77ccf9
+        }
+
+        private IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string content)
+        {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -34,8 +57,6 @@ namespace AspNetSandBox.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
-
-            //https://api.openweathermap.org/data/2.5/onecall?lat=46.4760&lon=24.0934&exclude=hourly,minutely&appid=33a7b8b2ffb63bb3514e58a43a77ccf9
         }
     }
 }
