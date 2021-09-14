be2ab6de1bf01e7da5b4d79bdfccaecfa174c85c
Septimiu Draghici <septimiu.draghici@principal33.com>
2021-09-14 12:10:28 +0300

    Fixed usings warnings

--- AspNetSandBox/Services/DbBooksRepository.cs
+++ AspNetSandBox/Services/DbBooksRepository.cs
@@ -1,10 +1,7 @@
-﻿using AspNetSandBox.Data;
-using AspNetSandBox.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using AspNetSandBox.Data;
+using AspNetSandBox.Models;
 
 namespace AspNetSandBox.Services
 {
