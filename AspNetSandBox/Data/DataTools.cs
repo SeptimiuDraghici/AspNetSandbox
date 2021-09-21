debbc4249d4ba43b4e24716488124373a9dc416d
Septimiu Draghici <septimiu.draghici@principal33.com>
2021-09-21 13:06:11 +0300

    Created two default books if there are none.

--- AspNetSandBox/Data/DataTools.cs
+++ AspNetSandBox/Data/DataTools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using AspNetSandBox.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,17 +10,29 @@ namespace AspNetSandBox.Data
     {
         public static void SeedData(this IApplicationBuilder app)
         {
-
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var applicationDbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
                 if (applicationDbContext.Book.Any())
                 {
-                    Console.WriteLine("The Books are here!");
+                    Console.WriteLine("The books are here!");
                 }
                 else
                 {
-                    Console.WriteLine("No Books found!");
+                    applicationDbContext.Add(new Book
+                    {
+                        Title = "Metro 2033",
+                        Author = "Dmitry Glukhovsky",
+                        Language = "Russian",
+                    });
+                    applicationDbContext.Add(new Book
+                    {
+                        Title = "Metro 2034",
+                        Author = "Dmitry Glukhovsky",
+                        Language = "Russian",
+                    });
+                    applicationDbContext.SaveChanges();
+                    Console.WriteLine("No books so added two default books!");
                 }
             }
         }
